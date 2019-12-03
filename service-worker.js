faf70b73a2de1c1c88257d91620ebf090ab40938
Grace <gkzhou@stanford.edu>
2019-12-02 19:37:39 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a03d45a9e1d6f824785b744f8183fb64.js"
+  "/precache-manifest.b856282eae3b6e4fb9977da3b3307447.js"
 );
 
 self.addEventListener('message', (event) => {
